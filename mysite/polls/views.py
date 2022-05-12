624141f1ae5a0c0f4b38eb1a0f572d3e87c9e636
Mohammad Abdullah <abdullah_22@hotmail.com>
2022-05-12 18:47:36 +0100

    HTML Form Complete

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 def index(request):
    latest_question_list = Question.objects.order_by('-pub_date')[:5]
@@ -16,6 +16,16 @@ def index(request):
    # return HttpResponse("Hello, world, You're at the polls index.")
 
 def detail(request, question_id):
+   question = get_object_or_404(Question,pk=question)
+   return render(request, 'polls/detail.html', {'question': question})
+
+   # return HttpResponse("You're looking at question %s" % question_id)
+
+def results(request, question_id):
+   question = get_object_or_404(Question, pk=question_id)
+   return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
    question = get_object_or_404(Question,pk=question)
    try:
       selected_choice = question.choice_set.get(pk=request.POST['choice'])
@@ -31,15 +41,5 @@ def detail(request, question_id):
       # Always return an HttpResponseRedirect after successfully dealing
       # with POST data.
       return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-   # return render(request, 'polls/details.html', {'question': question})
-
-   # return HttpResponse("You're looking at question %s" % question_id)
-
-def results(request, question_id):
-   question = get_object_or_404(Question, pk=question_id)
-   return render(request, 'polls/results.html', {'question': question})
-
-def vote(request, question_id):
-   return HttpResponse("You're voting on question %s." % question_id)
 
 
