6eee1064a584ec72f7fd32078b5ac25a9824968a
Mohammad Abdullah <abdullah_22@hotmail.com>
2022-05-16 15:36:45 +0100

    Finished Detail View Tests

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,4 +1,5 @@
 import datetime
+from urllib import response
 from venv import create
 
 from django.test import TestCase
@@ -102,4 +103,25 @@ class QuestionIndexViewTests(TestCase):
          [question2, question1],
       )
 
+
+class QuestionDetailViewTests(TestCase):
+   def test_future_question(self):
+      """
+      The details view of a question with a pub date in the future 
+      should return a 404 not found.
+      """
+      future_question = create_question(question_text="Future Question.", days=5)
+      url = reverse('polls:details', args(future_question.id,))
+      response = self.client.get(url)
+      self.assertEqual(response.status_code, 404)
+
+   def test_past_question(self):
+      """
+      The detail view of a question with a pub_date in the past
+      display the questiuons text.
+      """
+      past_question = create_question(question_text="Past Question", days=-5)
+      url = reverse('polls:detail', args=(past_question.id))
+      response = self.client.get(url)
+      self.assertContains(response, past_question.question_text)
 
