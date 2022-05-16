50ac0ec9660989d17659e02a8062626c843acc4b
Mohammad Abdullah <abdullah_22@hotmail.com>
2022-05-16 18:35:40 +0100

    Edited form layout of admin view

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 
-from .models import Question
-# Register your models here.
+from .models import Question, Choice
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+   model = Choice
+   extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+   # Form to show when adding a question
+   fieldsets = [
+      (None,               {'fields': ['question_text']}),
+      ('Date Information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+   ]
+   # Shows the choices inline with the question
+   inlines = [ChoiceInline]
+
+   # Displays all questions
+   list_display = ('question_text', 'pub_date', 'was_published_recently')
+   
+   # Filter for pub_date
+   list_filter = ['pub_date']
+
+   # Search function
+   search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
+
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,14 +1,24 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib import admin
 
 class Question(models.Model):
    question_text = models.CharField(max_length=200)
    pub_date = models.DateTimeField('date published')
 
+   # Overwrite the __str__ function from parent class
    def __str__(self):
       return self.question_text
 
+   # How to display the pub_date field in the admin portal
+   @admin.display(
+      boolean=True,
+      ordering='pub_date',
+      description='Published recently?',
+   )
+
+   # Only show questions that are older than now()
    def was_published_recently(self):
       now = timezone.now()
       return now - datetime.timedelta(days=1) <= self.pub_date <= now
